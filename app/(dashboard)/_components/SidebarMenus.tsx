0a726621fec1dd13783d08d481b66493a077a94a
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-09-29 19:43:10 +0530

    dynamic sidebar and navbar and created teachers page

--- app/(dashboard)/_components/SidebarMenus.tsx
+++ app/(dashboard)/_components/SidebarMenus.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
 import SidebarItem from "./SidebarItem";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   {
@@ -16,10 +17,28 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
+
 function SidebarMenus() {
+  const pathName = usePathname();
+
+  const isTeacherPage = pathName?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
   return (
     <>
-      {guestRoutes.map((route) => (
+      {routes.map((route) => (
         <SidebarItem
           key={route.href}
           href={route.href}
