aefbc90c336a2c3cbce4642ade6b6b08b2b3cdb5
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-10-12 20:09:00 +0530

    Add category options in search page

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -1,7 +1,16 @@
-function page() {
+import { db } from "@/lib/db";
+import Categories from "./_components/Categories";
+
+async function page() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
   return (
     <div>
-      <h1>Browse Page</h1>
+      <Categories items={categories} />
     </div>
   );
 }
