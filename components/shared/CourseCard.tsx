6cb93b0179c8f256d30d747fd155d383c63932f2
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-10-13 20:42:49 +0530

    Course grid and card ui.

---
+++ components/shared/CourseCard.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { IconBadge } from "./IconBadge";
+
+import { BookOpen } from "lucide-react";
+import { formatPrice } from "@/lib/format";
+
+type Props = {
+  id: string;
+  title: string;
+  price: number;
+  imageUrl: string;
+  chaptersLength: number;
+  progress: number | null;
+  category: string;
+};
+
+function CourseCard({
+  id,
+  title,
+  price,
+  imageUrl,
+  chaptersLength,
+  progress,
+  category,
+}: Props) {
+  return (
+    <Link href={`/course/${id}`}>
+      <div className="group p-3 hover:!shadow-sm h-full overflow-hidden border rounded-lg transition">
+        <div className="aspect-video overflow-hidden rounded-md relative w-full">
+          <Image fill src={imageUrl} alt={title} className="object-cover" />
+        </div>
+        <div className="flex flex-col pt-2">
+          <p className="text-lg md:text-base group-hover:text-sky-700 font-medium transition line-clamp-2">
+            {title}
+          </p>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="my-3 flex items-center text-sm md:text-xs gap-x-2">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+          </div>
+          {progress === null ? (
+            <p className="text-lg md:text-base font-medium text-slate-700">
+              {formatPrice(price)}
+            </p>
+          ) : (
+            <p>Progress</p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default CourseCard;
--- components/shared/CourseList.tsx
+++ components/shared/CourseList.tsx
@@ -1,4 +1,5 @@
 import { Category, Course } from "@prisma/client";
+import CourseCard from "./CourseCard";
 
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
@@ -13,9 +14,25 @@ type Props = {
 const CourseList = ({ items }: Props) => {
   return (
     <div className="py-6">
-      {items.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price!}
+            imageUrl={item.imageurl!}
+            chaptersLength={item.chapters?.length}
+            progress={item.progress}
+            category={item?.category?.name!}
+          />
+        ))}
+      </div>
+      {items?.length === 0 && (
+        <p className="text-lg text-muted-foreground mt-12 text-center">
+          No courses found
+        </p>
+      )}
     </div>
   );
 };
